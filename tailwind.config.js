3fc6a03903bac919f3df68de2ef699074929e76f
kevin45tech <kelvinmati45@gmail.com>
2022-07-02 22:40:16 +0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1440px",
+    },
+    extend: {
+      width: {
+        mobile: "90%",
+        container_width: "80%",
+      },
+      colors: {
+        mainColor: "#64bcf4",
+        hoverColor: "#5bacdf",
+      },
+    },
+  },
+  plugins: [],
+};
