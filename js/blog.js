82b3456c6bfd3bbfd9cc4e71c3a66b0acb6d9bb6
kelvinmati <kelvinmati45@gmail.com>
2024-10-18 23:40:29 +0300

    Completet refactor

--- js/blog.js
+++ js/blog.js
@@ -1,4 +1,4 @@
-// show more text on blog
+// show more text on 1st blog
 const moreArticle = document.querySelector(".more-article");
 const blogBtn = document.querySelector(".blog-btn");
 blogBtn.addEventListener("click", () => {
@@ -7,5 +7,17 @@ blogBtn.addEventListener("click", () => {
     blogBtn.innerHTML = "show less";
   } else {
     blogBtn.innerHTML = "show more";
+  }
+});
+
+// show more text on 2nd blog
+const moreArticle2 = document.querySelector(".more-article2");
+const blogBtn2 = document.querySelector(".blog-btn2");
+blogBtn2.addEventListener("click", () => {
+  moreArticle2.classList.toggle("expand-article2");
+  if (blogBtn2.innerHTML === "show more") {
+    blogBtn2.innerHTML = "show less";
+  } else {
+    blogBtn2.innerHTML = "show more";
   }
 });
